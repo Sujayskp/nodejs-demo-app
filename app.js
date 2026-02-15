798633546530d9ec422dd77e5b6b542d6ee82b03
Sujayskp <sujayskp123@gmail.com>
2026-02-15 18:12:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker + Jenkins 🚀");
+    res.send("Hello DevOps Internship 🚀");
 });
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
-});
-
-const app = expressss();
-
-app.get("/", (req, res) => {
-  res.send("Hello DevOps Internship 🚀");
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
